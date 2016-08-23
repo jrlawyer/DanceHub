079fbc84166bcbca9fae72e16fd261c5a818625e
Jennifer <jrlawyer@gmail.com>
2016-08-23 14:24:16 -0400

    I added the event model.

---
+++ DanceHub/Models/Type.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DanceHub.Models
+{
+    public class Type
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+
+}   
